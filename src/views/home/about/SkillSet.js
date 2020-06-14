d5fe929c65f18f2c7394caca60c27cb998a1ae75
debihiga <debihiga@gmail.com>
2020-06-14 14:41:46 -0300

    Added small changes in skill descriptions.

--- src/views/home/about/SkillSet.js
+++ src/views/home/about/SkillSet.js
@@ -33,36 +33,78 @@ function createData(logoPath, name, description) {
 
 const skills = [
   createData("/images/logos/java.jpg", "Java",
-    <span>Backend developer of image processing, banking and 24/7 global, desktop and cloud applications,
-    using Spring Boot, Drools, Maven, Jules, Jenkins, REST and websocket services. 
-    I have been part of Agile cross teams from USA, UK, India and Argentina. 
-    I also love solving coding challenges in <a target="_blank" href="https://leetcode.com/debihiga/" >
-    LeetCode
-    </a> using Java.</span>),
+    <span>
+      Backend developer of banking and 24/7 global cloud applications,
+      using Spring Boot, Drools, Maven, Jules, Jenkins, REST, and WebSocket services. 
+      Collaborated in Agile cross teams from the USA, UK, India, and Argentina. 
+      Built from scratch, and completely by me, image processing projects for object detection and recognition.
+      Responsible for the development, deployment, and support of each application. 
+      <a target="_blank" href="https://leetcode.com/debihiga/" >
+      LeetCode 
+      </a> coding challenges lover &lt;3.
+    </span>),
   createData("/images/logos/react.jpg", "React",
-    <span>Frontend developer in React, using Material UI. 
-    I focus on the costumers and their needs, 
-    trying to enhance also the UX side of the applications. 
-    Ah! Also this website is built in React :)</span>),
+    <span>
+      Frontend developer in React, using Material UI and Redux. 
+      Focused on our costumers, their needs, and the enhancement of the UX side of our applications.
+      Ah! Also, this website is built in React :)
+    </span>),
   createData("/images/logos/python.jpg", "Python",
-    <span>Backend developer of image processing desktop applications using PyQT and OpenCV.</span>),
+    <span>
+      Developed image processing desktop applications using PyQt and OpenCV. 
+      In addition to this, I have built small scripts for 
+      data collection from sensors and transmission to servers in single-board computers, 
+      like the Raspberry Pi, Beaglebone, Arduino, etc.
+    </span>),
   createData("/images/logos/mysql.jpg", "MySQL",
-    <span>Database developer of relational tables and store procedures, 
-    using also Pentaho transformations and jobs. 
-    I have experience in Oracle SQL and SQLite, too.</span>),
+    <span>
+      Built complex relational tables and store procedures, 
+      using also Pentaho transformations and jobs. 
+      I have experience in Oracle SQL and SQLite, too.
+    </span>),
   createData("/images/logos/opencv.jpg", "OpenCV",
-    <span>I have used IP cameras and also LIDARs to analyze and process data for object recognition and detection. 
-    This is one of my favourite fields :)</span>),
+    <span>
+      This is one of my favourite fields :)
+      I have used IP cameras and also LIDARs to analyze and process data for 
+      object detection and recognition. 
+    </span>),
   createData("/images/logos/android.jpg", "Android",
-    <span>For one of my previous jobs, I developed two Android applications that are currently in Google Play Store.</span>),
+    <span>
+      Developed from scratch and completely by me, 
+      Android applications that some of them are currently in Google Play Store:
+      <ul>
+        <li>
+          <a target="_blank" href="https://play.google.com/store/apps/details?id=com.ingesys.controlremotoba90ip" >
+            Parking barrier remote control
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://play.google.com/store/apps/details?id=com.ingesys.carteltarifario" >
+            Price table customizer
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://youtu.be/yxUsVv4iVQk" >
+            Watering scheduler
+          </a>
+        </li>
+      </ul>
+    </span>),
   createData("/images/logos/reveng.jpg", "Reverse Engineering",
-    <span>During an internship, I have done a firmware reverse engineering research, found 2 vulnerabilities and presented them in the ekoparty Security Conference.</span>),
+    <span>
+      During an internship, I have done a firmware reverse engineering research, found 2 vulnerabilities, and presented them in the ekoparty Security Conference.
+    </span>),
   createData("/images/logos/electronics.jpg", "Electronics",
-    <span>On my previous jobs, I have developed embedded electronic systems in C connecting with sensors like GPS, ultrasounds, accelerometers, gyroscopes and magnetometers. 
-    I have experience in Raspberry, Beaglebone and Andruino, too.</span>),
+    <span>
+      Developed embedded electronic systems in C, connecting with sensors like GPS, ultrasounds, accelerometers, gyroscopes, and magnetometers.
+      Furthermore, I have done the assembly, reparation, and testing of electronic boards. 
+      I have experience also in single-board computers, like the Raspberry Pi, Beaglebone, Arduino, etc.
+    </span>),
   createData("/images/logos/others.jpg", "Others",
-    <span>In my previous jobs, I have also developed entire projects in C++ and PHP. 
-    I have also built small applications in Matlab.</span>),
+    <span>
+      In my first jobs, I have also developed from scratch and completely by me, projects in C++ and PHP. 
+      I have also built small applications in Matlab.
+    </span>),
 ];
 
 export default function SkillSet(props) {
